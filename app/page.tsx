01cffdc20ab4fa588fb81866678f355e40afcbe2
Eldad Tolla <etolla@randolphcollege.edu>
2026-02-15 17:49:46 -0500

    Hello World initial deployment

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-black">
+      <h1 className="text-5xl font-bold text-white">
+        Hello World – Eldad
+      </h1>
+    </main>
+  );
+}
